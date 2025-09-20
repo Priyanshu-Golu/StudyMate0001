b9afbc7f5c623ff122372457dbc003a891e5af63
Priyanshu-Golu <priyanshuwrath@gmail.com>
2025-09-20 15:32:39 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.routes import auth, pdf
+from app.RAG import rag
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(pdf.router)
+app.include_router(rag.router)
 
 @app.on_event("startup")
 async def startup():
